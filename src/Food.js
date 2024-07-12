8d3f9790b21db9ebf6a3120d2d197f7466a2e99c
MoHAMED <MSSS898123@gmail.com>
2024-07-12 17:05:00 +0300

    update price style

--- src/Food.js
+++ src/Food.js
@@ -6,7 +6,7 @@ function Food(a) {
       <img src={a.img} alt="Food" className="food-image" />
       <h2 className="food-title">{a.title}</h2>
       <p className="food-description">{a.description}</p>
-      <p className="food-description">{a.price}</p>
+      <p className="food-price">{a.price}</p>
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -12,33 +12,33 @@ const Data=[
     {img : require('./cherries.jpg'),
         title :"Cherries, interrupted",
         description : 'Once again, some random text to lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum.',
-        price : "18.99$"
+        price : "19.99$"
     },
 
     {img : require('./wine.jpg'),
         title :"Once Again, Robust Wine and Vegetable Pasta",
         description : 'Lorem ipsum text praesent tincidunt .',
-        price : "18.99$"
-    },
-    {img : require('./croissant.jpg'),
-        title :"Le French",
-        description : 'Lorem ipsum text praesent tincidunt ipsum lipsum.',
-        price : "18.99$"
-    },
-    {img : require('./sandwich (1).jpg'),
-        title :"The Perfect Sandwich, A Real Classic",
-        description : 'Lorem ipsum text praesent tincidunt .',
-        price : "18.99$"
-    },
-    {img : require('./salmon.jpg'),
-        title :"Salmon For Your Skin",
-        description : 'Lorem ipsum text praesent tincidunt ipsum lipsum.',
-        price : "18.99$"
+        price : "40.99$"
     },
     {img : require('./popsicle.jpg'),
         title :"All I Need Is a Popsicle",
         description : 'Lorem ipsum text praesent tincidunt ipsum lipsum.',
-        price : "18.99$"
+        price : "15.99$"
+    },
+    {img : require('./salmon.jpg'),
+        title :"Salmon For Your Skin",
+        description : 'Lorem ipsum text praesent tincidunt ipsum lipsum.',
+        price : "100$"
+    },
+    {img : require('./sandwich (1).jpg'),
+        title :"The Perfect Sandwich, A Real Classic",
+        description : 'Lorem ipsum text praesent tincidunt .',
+        price : "20$"
+    },
+    {img : require('./croissant.jpg'),
+        title :"Le French",
+        description : 'Lorem ipsum text praesent tincidunt ipsum lipsum.',
+        price : "18.50$"
     },
 ]
 export default Data 
